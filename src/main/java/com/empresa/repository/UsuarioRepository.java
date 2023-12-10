a03e94627827a01d6b17811059e66c060f24f451
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-12-10 10:09:44 -0500

    finalizado

--- src/main/java/com/empresa/repository/UsuarioRepository.java
+++ src/main/java/com/empresa/repository/UsuarioRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.empresa.entidades.Opcion;
+import com.empresa.entidades.Pasatiempo;
 import com.empresa.entidades.Rol;
 import com.empresa.entidades.Usuario;
 
@@ -22,5 +23,9 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 	public abstract List<Rol> traerRolesDeUsuario(@Param("var_idUsuario")int idUsuario);
 	
 	public abstract Usuario findByLogin(String login);
+
+	@Query("Select r.pasatiempo from UsuarioHasPasatiempo r where r.usuario.idUsuario = ?1")
+	public abstract List<Pasatiempo> traerPasatiempoDeUsuario(int idUsuario);
+
 	
 }
